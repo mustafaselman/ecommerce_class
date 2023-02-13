8d74e5826f963b19e0a73fd82875e040b43e4fc7
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-13 14:46:10 +0300

    2-1-(App.js)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Footer } from "./components";
-import Header from "./components/header/Header";
+import { Footer, Header} from "./components";
 import { Contact, Home } from "./pages";
 
 const App = () => {
