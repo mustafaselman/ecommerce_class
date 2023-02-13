eccf84f3ed4d56c872e31939f9e1ae20ed0436e8
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-13 14:47:45 +0300

    3-1-(components/header/Header.js)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,24 @@
 //// tüm linklerin olduğu başlık kısmı
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+  const logo = (
+    <div className={styles.logo}>
+      <Link to="/">
+        <h2>
+          e<span>Shop</span>.
+        </h2>
+      </Link>
+    </div>
+  );
+
+  return (
+    <header>
+      <div className={styles.header}>{logo}</div>
+    </header>
+  );
+};
+
+export default Header;
