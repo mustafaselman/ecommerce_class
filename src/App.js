4f033d1f67536d880b702ab94978a71ee2bdd15e
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-13 14:17:30 +0300

    2-1-(App.js)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Footer } from "./components";
+import Header from "./components/header/Header";
+import { Contact, Home } from "./pages";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
