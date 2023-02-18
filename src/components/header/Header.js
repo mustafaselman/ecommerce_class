ef327963bcad57d59086312629f374b417dc3dad
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-19 00:27:25 +0300

    5-2-(components/header/Header.js)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,22 @@
 //// tüm linklerin olduğu başlık kısmı
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 import { FaShoppingCart } from "react-icons/fa";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 const Header = () => {
+  // responsive de yan menüyü(nav) gizleme/gösterme için oluşturuldu
+  const [showMenu, setShowMenu] = useState(false);
+
+  // responsive yan menüyü açan hamburger butonu için açma kapama fonksiyonu oluşturuldu
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  // boşluğa bastığımızda responsive yan menünün kapanması için oluşturuldu
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
   const logo = (
     <div className={styles.logo}>
       <Link to="/">
@@ -29,7 +41,21 @@ const Header = () => {
     <header>
       <div className={styles.header}>
         {logo}
-        <nav>
+        {/* responsive olunca sola gizlenen nav "show-nav" ile sağa kaydırılıyor, hide-nav ile tekrar sola alınıyor*/}
+        <nav
+          className={
+            showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+          }
+        >
+          {/* responsivede sola yerleşen navın üzerine "nav-wrapper" ile aynı boyutta bir siyah ekran ekleniyor. Bu siyah ekran, nav sağa kayınca "show-nav-wrapper" ile %100 daha fazla sağa kayarak ekranın sağ kısmını dolduruyor. */}
+          <div
+            className={
+              showMenu
+                ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                : `${styles["nav-wrapper"]}`
+            }
+            onClick={hideMenu}
+          ></div>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -48,7 +74,7 @@ const Header = () => {
         </nav>
         <div className={styles["menu-icon"]}>
           {cart}
-          <HiOutlineMenuAlt3 size={28} />
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
         </div>
       </div>
     </header>
