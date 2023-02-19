e8423e3e0fa3996b050e35e23292c3f34aeced2d
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-19 07:22:46 +0300

    8-1-(App.js)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Footer, Header} from "./components";
-import { Contact, Home } from "./pages";
+import { Contact, Home, Login, Register, Reset } from "./pages";
 
 const App = () => {
   return (
@@ -11,6 +11,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/contact" element={<Contact/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/reset" element={<Reset/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
