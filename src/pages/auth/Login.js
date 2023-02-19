7de9bde4c3495a2789b975ea7b6049f9c4a9c411
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-19 14:31:51 +0300

    9-1-(pages/auth/Login.js)

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,36 @@
 //// kayıtlı kullanıcılar için giriş sayfası
 import React from 'react'
-
+import styles from "./auth.module.scss"
+import loginImg from "../../assets/login.png"
+import { Link } from 'react-router-dom'
+import { FaGoogle } from "react-icons/fa"
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className={`container ${styles.auth}`}>
+      
+      <div className={styles.img}>
+        <img src={loginImg} alt="Login" width="400"/>
+      </div>
+      <div className={styles.form}>
+        <h2>Login</h2>
+        <form>
+          <input type="text" placeholder="Email" required />
+          <input type="password" placeholder='Password' required />
+          {/* bu classlar index.css klasöründe olduğu için otomatik olarak etkilediği tüm childler kullanır. */}
+          <button className="--btn --btn-primary --btn-block">Login</button>
+          <div className={styles.links}>
+            <Link to="/reset">Reset Password</Link>
+          </div>
+          <p>-- or --</p>
+        </form>
+        <button className="--btn --btn-danger --btn-block"><FaGoogle color="#fff"/>Login With Google</button>
+        <span className={styles.register}>
+          <p>Don't have an account</p>
+          <Link to="/register">Register</Link>
+        </span>
+      </div>
+
+    </section>
   )
 }
 
