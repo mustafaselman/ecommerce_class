7c984cb1720770b75cb88de37c4a83f4ed1898cd
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-19 19:26:54 +0300

    14-1- (components/loader/Loader.js)

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,10 +1,17 @@
 //// bazı işlemlerde beklemek gerektiğinde loader.gif çalıştırılır
-import React from 'react'
+import React from "react";
+import styles from "./Loader.module.scss";
+import loaderImg from "../../assets/loader.gif";
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    // tam ekranı kaplama için "wrapper", loader gif i ortalamak için "loader" classı kullandık.
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <img src={loaderImg} alt="Loading..." />
+      </div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
