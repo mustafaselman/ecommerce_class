cec49bab485734ae268c321387b956606c06ea0d
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-19 18:10:25 +0300

    13-2- (firebase/config.js)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,13 @@
 //// burada google firebase ile etkileşime buradan girilecek.
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC541Olekoe2TDFO1CbIDMQoUGoGkdIXZk",
+  authDomain: "eshop-36c71.firebaseapp.com",
+  projectId: "eshop-36c71",
+  storageBucket: "eshop-36c71.appspot.com",
+  messagingSenderId: "667018684999",
+  appId: "1:667018684999:web:0a92c24bfca3352a99df62"
+};
+
+const app = initializeApp(firebaseConfig);
