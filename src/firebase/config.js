7037465bf0639e561fa92aeb5211e4e98a271cb0
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-19 18:56:30 +0300

    13-3- (firebase/config.js)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,13 @@
 //// burada google firebase ile etkileşime buradan girilecek.
 import { initializeApp } from "firebase/app";
+// authentication işlemleri için (giriş kontrol,yetkilendirme)
+import { getAuth } from "firebase/auth"
+// firestore database erişmek için (verilerin kayıt yeri)
+import { getFirestore } from "firebase/firestore"
+// storage erişimi için (resimlerin kayıt yeri)
+import { getStorage } from "firebase/storage"
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyC541Olekoe2TDFO1CbIDMQoUGoGkdIXZk",
   authDomain: "eshop-36c71.firebaseapp.com",
   projectId: "eshop-36c71",
@@ -11,3 +17,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+export default app
