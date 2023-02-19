51da8d8393bd089bbf4272d62c663b73d7f7ffe8
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-19 19:38:46 +0300

    14-3- (component/loader.Loader.js)

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -2,15 +2,16 @@
 import React from "react";
 import styles from "./Loader.module.scss";
 import loaderImg from "../../assets/loader.gif";
+import ReactDOM from "react-dom"
 
 const Loader = () => {
-  return (
+  return ReactDOM.createPortal(
     // tam ekranı kaplama için "wrapper", loader gif i ortalamak için "loader" classı kullandık.
     <div className={styles.wrapper}>
       <div className={styles.loader}>
         <img src={loaderImg} alt="Loading..." />
       </div>
-    </div>
+    </div>, document.getElementById("loader")
   );
 };
 
