e6f645208cc062d37c363ec720a68d28180799e4
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-19 15:38:18 +0300

    10-1-(components/card/Card.js)

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,10 @@
 //// oluşturulan tüm cardlar için kullanılan en dış div ve className belirleme olayını tekrarlamamak adına yazılan bir component
-import React from 'react'
+import React from "react";
+import styles from "./Card.module.scss";
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+// Card içerisine aldığımız componentlere(children) card yapısı oluşturmak ve onların kullandığı custom class ları "cardClass" olarak belirtmek için oluşturuldu. Amaç ortak kodları toplu bir yapıya kavuşturarak 
+const Card = ({ children, cardClass }) => {
+  return <div className={`${styles.card} ${cardClass}`}>{children}</div>;
+};
 
-export default Card
+export default Card;
