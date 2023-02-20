727b6ab1f37f6bda47b0d362dc51419f2da94680
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-20 10:25:31 +0300

    17-1- (pages/auth/Login.js)

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,11 +1,20 @@
 //// kayıtlı kullanıcılar için giriş sayfası
-import React from "react";
+import React, { useState } from "react";
 import styles from "./auth.module.scss";
 import loginImg from "../../assets/login.png";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import Card from "../../components/card/Card";
 const Login = () => {
+
+  // formdaki inputlara ait state ler
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+  const loginUser = (e) => {
+    e.preventDefault()
+    console.log(email,password)
+  }
   return (
     <section className={`container ${styles.auth}`}>
       <div className={styles.img}>
@@ -13,11 +22,11 @@ const Login = () => {
       </div>
       <Card cardClass={styles.form}>
         <h2>Login</h2>
-        <form>
-          <input type="text" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
+        <form onSubmit={loginUser}>
+          <input type="text" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
           {/* bu classlar index.css klasöründe olduğu için otomatik olarak etkilediği tüm childler kullanır. */}
-          <button className="--btn --btn-primary --btn-block">Login</button>
+          <button type="submit" className="--btn --btn-primary --btn-block">Login</button>
           <div className={styles.links}>
             <Link to="/reset">Reset Password</Link>
           </div>
