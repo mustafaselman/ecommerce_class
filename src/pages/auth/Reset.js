4bdbc5911abd52377fcbc1efb247b8d31f98d070
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-21 14:17:44 +0300

    20-1- (pages/auth/Reset.js)

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,11 +1,19 @@
 ////şifre resetleme sayfası
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./auth.module.scss"
 import resetImg from "../../assets/forgot.png"
 import Card from '../../components/card/Card'
 import { Link } from 'react-router-dom'
 
 const Reset = () => {
+
+  const [ email, setEmail ] = useState("")
+
+  const resetPassword = (e) => {
+    e.preventDefault()
+    alert(email)
+}
+
   return (
     <section className={`container ${styles.auth}`}>
     <div className={styles.img}>
@@ -13,10 +21,10 @@ const Reset = () => {
     </div>
     <Card cardClass={styles.form}>
       <h2>Reset</h2>
-      <form>
-        <input type="text" placeholder="Email" required />
+      <form onSubmit={resetPassword}>
+        <input type="text" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
         {/* bu classlar index.css klasöründe olduğu için otomatik olarak etkilediği tüm childler kullanır. */}
-        <button className="--btn --btn-primary --btn-block">Reset Password</button>
+        <button type="submit" className="--btn --btn-primary --btn-block">Reset Password</button>
         <div className={styles.links}>
           <p>
             <Link to="/login">- Login</Link>
