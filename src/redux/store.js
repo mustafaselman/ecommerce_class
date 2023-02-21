b4a866c4ce403e80577b2930c214635f31d0f0e8
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-21 15:59:10 +0300

    22-1- (redux/store.js)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,15 @@
 //// burası redux store kayıt yeri
+import {configureStore, combineReducers} from "@reduxjs/toolkit"
+
+// burası reucerlarımızı ekleyeceğimiz yer
+const rootReducer = combineReducers({
+    
+});
+
+// burade reducerlar store a kaydediliyor.
+const store = configureStore({
+    reducer: rootReducer,
+   
+})
+
+export default store
