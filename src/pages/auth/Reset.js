1f700cda0e2ac9e31f23681af77e169b85922121
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-21 14:43:47 +0300

    20-3- (pages/auth/Reset.js)

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -7,23 +7,30 @@ import { Link } from 'react-router-dom'
 import { sendPasswordResetEmail } from 'firebase/auth'
 import { auth } from '../../firebase/config'
 import { toast } from 'react-toastify'
+import Loader from '../../components/loader/Loader'
 
 const Reset = () => {
 
   const [ email, setEmail ] = useState("")
+  const [ isLoading, setIsLoading ] = useState(false)
 
   const resetPassword = (e) => {
     e.preventDefault();
+    setIsLoading(true)
     sendPasswordResetEmail(auth, email)
       .then(() => {
+        setIsLoading(false)
         toast.success("Check your email for a reset link");
       })
       .catch((error) => {
+        setIsLoading(false)
         toast.error(error.message);
       });
   };
 
   return (
+    <>
+    {isLoading && <Loader />}
     <section className={`container ${styles.auth}`}>
     <div className={styles.img}>
       <img src={resetImg} alt="Login" width="400" />
@@ -45,6 +52,7 @@ const Reset = () => {
       </form>
     </Card>
   </section>
+    </>
   )
 }
 
