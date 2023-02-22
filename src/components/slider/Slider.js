5cf432d20997e26c283f1264726d7a5a9a02087b
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-22 15:56:19 +0300

    29-1- (components/slider/Slider.js)

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,5 +1,5 @@
 //// home sayfasındaki slider i yapacağımız yer
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Slider.scss"
 import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai"
 import {sliderData} from "./slider-data"
@@ -11,11 +11,33 @@ const Slider = () => {
   // data genişliğimiz 4 tür fakat indexler 0,1,2,3 şeklindedir.
   const slideLength = sliderData.length
 
+  // otomatik kaydırmayı kontrol edebiliriz.
+  const autoScroll = true;
+
+  // kaydırma süreesini kontrol edebiliriz.
+  let intervalTime = 5000;
+
   // arrow right a bastığımızda sağa slide değişimi için kullanılır. eğer currentslide 3 ise bir sonraki 0 olacak yoksa 1 arttır
   const nextSlide = () => {setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1)}
 
   // arrow left e bastığımızda sola slide değişimi için kullanılır. eğer currentslide 0 ise bir sonraki 3 olacak yoksa 1 azalt
   const prevSlide = () => {setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1)}
+
+  // sayfa her açılışında slider ı sıfırlıyor
+  useEffect(() => {
+    setCurrentSlide(0)
+  }, [])
+  
+  // otomatik slider ı kontrol eder. autoscroll ve intervaltime a göre ayarlanabilir. nextSlide fonksiyonu gibi çalışır, currentslide sürekli ilerler.interval her saymada sıfırlanır. currentSlide sürekli değiştiği için useEffect sürekli çalışır.
+  useEffect(() => {
+    let slideInterval;
+    if(autoScroll) {
+      slideInterval = setInterval(() => {
+        setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1)
+      },intervalTime);
+    }
+    return () => clearInterval(slideInterval);
+  }, [autoScroll,intervalTime,currentSlide,slideLength])
 
   return (
     <div className="slider">
