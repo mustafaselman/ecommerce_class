d04cfefb5e5c230b81bc836c3b6722e9efe181e2
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-23 09:39:38 +0300

    30-1-(components/adminOnlyRoute/AdminOnlyRoute.js)

--- src/components/adminOnlyRoute/AdminOnlyRoute.js
+++ src/components/adminOnlyRoute/AdminOnlyRoute.js
@@ -1,10 +1,22 @@
 ////admin olmayıp da admin sayfasına erişmek isteyenler için permission denied sayfası
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectEmail } from '../../redux/slice/authSlice'
 
 const AdminOnlyRoute = () => {
   return (
     <div>AdminOnlyRoute</div>
   )
+}
+
+export const AdminOnlyLink = ({children}) => {
+  const userEmail = useSelector(selectEmail)
+  
+  if(userEmail === "user1@gmail.com"){
+    return children
+  }
+  
+  return null
 }
 
 export default AdminOnlyRoute
