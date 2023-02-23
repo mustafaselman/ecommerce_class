85d95e0bf5d7bf981db4c6d40eb9ba432f90b19b
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-23 12:01:20 +0300

    33-1- (components/admin/navbar/Navbar.js)

--- src/components/admin/navbar/Navbar.js
+++ src/components/admin/navbar/Navbar.js
@@ -1,10 +1,53 @@
 ////admin panelindeki sol navbar
 import React from 'react'
+import { selectUserName } from '../../../redux/slice/authSlice'
+import styles from "./Navbar.module.scss"
+import {useSelector} from "react-redux"
+import {FaUserCircle} from "react-icons/fa"
+import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+// nav içindeki navLinkler tıklandığında isactive true olur. Bizde bu durumu kullanarak bir class ekledikki seçilen link belli olsun(activelink console.log yaparak burdan kontrol edilebilir a => console.log(a))
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "")
+
+const Navbar = () =>
+{
+    const userName = useSelector(selectUserName)
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.user}>
+                <FaUserCircle size={40} color="#fff"/>
+                <h4>{userName}</h4>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                      <NavLink to="/admin/home" className={activeLink}>
+                        Home 
+                      </NavLink>
+                      
+                    </li>
+                    <li>
+                      <NavLink to="/admin/all-products" className={activeLink}>
+                        All Products 
+                      </NavLink>
+                      
+                    </li>
+                    <li>
+                      <NavLink to="/admin/add-product" className={activeLink}>
+                        Add Product 
+                      </NavLink>
+                      
+                    </li>
+                    <li>
+                      <NavLink to="/admin/orders" className={activeLink}>
+                        Orders 
+                      </NavLink>
+                      
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
