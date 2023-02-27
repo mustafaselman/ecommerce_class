5c3577a8176e321f583f264d1af45b48ab584651
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-27 13:28:25 +0300

    40-1-components/admin/viewProducts/ViewProducts.js

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,7 +1,23 @@
 //// admin panelindeki allproducts componenti
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
 const ViewProducts = () => {
+
+  const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  
+  const getProducts = () => {
+    setIsLoading(true)
+    try {
+
+    }
+    catch(error) {
+      setIsLoading(false)
+      toast.error(error.message)
+    }
+  };
+
   return (
     <div>ViewProducts</div>
   )
