85f311e7010033934857a7d0a0627aa7ea4ce563
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-27 14:41:40 +0300

    41-1-components/admin/viewProducts/ViewProducts.js

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -2,8 +2,12 @@
 import { onSnapshot, orderBy, query } from 'firebase/firestore';
 import { collection } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { db } from '../../../firebase/config';
+import Loader from '../../loader/Loader';
+import styles from "./ViewProducts.module.scss";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
 const ViewProducts = () => {
 
@@ -31,6 +35,7 @@ const ViewProducts = () => {
         }));
         // console.log(allProducts);
         setProducts(allProducts);
+        setIsLoading(false)
       })
 
     }
@@ -41,7 +46,62 @@ const ViewProducts = () => {
   };
 
   return (
-    <div>ViewProducts</div>
+    <>
+    {isLoading && <Loader />}
+    <div className={styles.table}>
+      <h2>All Products</h2>
+      {products.length === 0 ? (
+        <p>No product found.</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>s/n</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) =>
+            {
+              const { id, name, price, imageURL, category } = product;
+              return (
+
+                <tr key={id}>
+                  <td>
+                    {index + 1}
+                  </td>
+                  <td>
+                    <img src={imageURL} alt={name} style={{ width: "100px" }}></img>
+                  </td>
+                  <td>
+                    {name}
+                  </td>
+                  <td>
+                    {category}
+                  </td>
+                  <td>
+                    {`$${price}`}
+                  </td>
+                  <td className={styles.icons}>
+                    <Link to="/admin/add-product/">
+                      <FaEdit size={20} color="green" />
+                    </Link>
+                    &nbsp;
+                    <FaTrashAlt size={18} color="red" />
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      )}
+    </div>
+  </>
+
   )
 }
 
