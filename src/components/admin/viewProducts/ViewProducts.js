881ce04345438f98f5921e1a74a3647b961c2325
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-28 09:16:05 +0300

    44-3-components/admin/viewProducts/ViewProducts.js

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -10,11 +10,15 @@ import styles from "./ViewProducts.module.scss";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import { deleteObject, ref } from 'firebase/storage';
 import Notiflix from 'notiflix';
+import { useDispatch } from 'react-redux';
+import { STORE_PRODUCTS } from '../../../redux/slice/productSlice';
 
 const ViewProducts = () => {
 
   const [products, setProducts] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+
+  const dispatch = useDispatch();
 
   useEffect(()=> {
     getProducts();
@@ -38,6 +42,7 @@ const ViewProducts = () => {
         // console.log(allProducts);
         setProducts(allProducts);
         setIsLoading(false)
+        dispatch(STORE_PRODUCTS(allProducts))
       })
     }
     catch(error) {
