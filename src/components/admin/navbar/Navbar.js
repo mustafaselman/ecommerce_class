d212db7dbcf52f2e90a589db0552f32817a64989
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-28 09:45:25 +0300

    45-2- (components/admin/navbar/Navbar.js)

--- src/components/admin/navbar/Navbar.js
+++ src/components/admin/navbar/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () =>
                       
                     </li>
                     <li>
-                      <NavLink to="/admin/add-product" className={activeLink}>
+                      <NavLink to="/admin/add-product/ADD" className={activeLink}>
                         Add Product 
                       </NavLink>
                       
