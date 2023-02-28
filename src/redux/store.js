e78ee8d78901c309370683cf517edbf8148d47d5
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-28 09:05:25 +0300

    44-2- (redux/store.js)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 //// burası redux store kayıt yeri
 import {configureStore, combineReducers} from "@reduxjs/toolkit"
 import authReducer from "./slice/authSlice"
+import productReducer from "./slice/productSlice"
 
 // burası reucerlarımızı ekleyeceğimiz yer
 const rootReducer = combineReducers({
     auth: authReducer,
+    product: productReducer,
 });
 
 // burade reducerlar store a kaydediliyor.
