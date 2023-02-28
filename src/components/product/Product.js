bda13c979eb7a8a689d0e6dc648340bd2cc3ea10
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-02-28 15:12:50 +0300

    48-2- (components/product/Product.js)

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,9 +1,21 @@
 ////home sayfasında productlist ve productfilterin beraber sergilendiği yer
 import React from 'react'
+import styles from "./Product.module.scss"
+import ProductFilter from './productFilter/ProductFilter'
+import ProductList from './productList/ProductList'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <section>
+      <div className={`container ${styles.product}`}>
+        <aside className={styles.filter}>
+          <ProductFilter/> 
+        </aside>
+        <div className={styles.content}>
+          <ProductList/>
+        </div>
+      </div>
+    </section>
   )
 }
 
