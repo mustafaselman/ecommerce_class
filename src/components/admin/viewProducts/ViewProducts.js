e6c754fbb70b29be4a767a294fe5e7cd3fb3f8ec
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-01 08:18:52 +0300

    50-1-components/admin/viewProducts/ViewProducts.js

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -10,49 +10,27 @@ import styles from "./ViewProducts.module.scss";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import { deleteObject, ref } from 'firebase/storage';
 import Notiflix from 'notiflix';
-import { useDispatch } from 'react-redux';
-import { STORE_PRODUCTS } from '../../../redux/slice/productSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectProducts, STORE_PRODUCTS } from '../../../redux/slice/productSlice';
+import useFetchCollection from '../../../customHooks/useFetchCollection';
 
 const ViewProducts = () => {
 
-  const [products, setProducts] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const { data, isLoading } = useFetchCollection("products")
+  
+  const products = useSelector(selectProducts);
 
   const dispatch = useDispatch();
 
-  useEffect(()=> {
-    getProducts();
-  },[])
-  
-  const getProducts = () => {
-    setIsLoading(true)
-    try {
-      // belirli bir koleksiyonu almak için kullanılır
-      const productsRef = collection(db, "products");
-      // seçtiğimiz koleksiyonu sıralamak için kullanılır.
-      const q = query(productsRef, orderBy("createdAt", "desc"));
-      // gerçek zamanlı anlık olarak sıraladığımız bu verileri dinlemek için kullanılır.
-      onSnapshot(q, (snapshot) => {
-        // console.log(snapshot)
-        // console.log(snapshot.docs[0].data())
-        const allProducts = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data()
-        }));
-        // console.log(allProducts);
-        setProducts(allProducts);
-        setIsLoading(false)
-        dispatch(STORE_PRODUCTS({
-          products : allProducts,
-        }))
+  //data değiştikçe redux sürekli güncellenecektir.
+  useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
       })
-    }
-    catch(error) {
-      setIsLoading(false)
-      toast.error(error.message)
-    }
-  };
-
+    );
+  }, [dispatch, data]);
+  
   const confirmDelete = (id, imageURL) => {
     Notiflix.Confirm.show(
       'Delete Product!!!',
