fa9309d5b8db999770a12dd00de36839400b5789
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-03 13:58:14 +0300

    57-3- (customHooks/useFetchDocument.js)

--- src/customHooks/useFetchDocument.js
+++ src/customHooks/useFetchDocument.js
@@ -1,10 +1,34 @@
 //// google firebase içerisinde 3 koleksiyon tutulacak: products, orders, reviews. Bu koleksiyonlara ait dökümanların bireysel bilgilerine ihtiyaç duyulduğunda bu hook kullanılacak.
-import React from 'react'
+import { doc, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { db } from "../firebase/config";
 
-const useFetchDocument = () => {
-  return (
-    <div>useFetchDocument</div>
-  )
-}
+const useFetchDocument = (collectionName, documentID) => {
+  const [document, setDocument] = useState(null);
 
-export default useFetchDocument
+  useEffect(() => {
+    try {
+      const getDocument = async () => {
+        const docRef = doc(db, collectionName, documentID);
+
+        onSnapshot(docRef, (doc) => {
+          console.log("Document data:", doc.data());
+          const obj = {
+            id: documentID,
+            ...doc.data(),
+          };
+          setDocument(obj);
+        });
+      };
+      getDocument();
+    } catch (error) {
+      toast.error(error);
+    }
+    
+  }, [collectionName, documentID]);
+
+  return document;
+};
+
+export default useFetchDocument;
