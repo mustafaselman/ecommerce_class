0c60e7be046ee1015c22728636550bd17ca15080
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-07 13:32:04 +0300

    67-1- (components/product/Product.js)

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,5 +1,5 @@
 ////home sayfasında productlist ve productfilterin beraber sergilendiği yer
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import useFetchCollection from '../../customHooks/useFetchCollection'
 import { GET_PRICE_RANGE, selectProducts, STORE_PRODUCTS } from '../../redux/slice/productSlice'
@@ -7,11 +7,15 @@ import styles from "./Product.module.scss"
 import ProductFilter from './productFilter/ProductFilter'
 import ProductList from './productList/ProductList'
 import spinnerImg from "../../assets/spinner.gif"
+import {FaCogs} from "react-icons/fa"
 
 const Product = () => {
 
   const { data, isLoading } = useFetchCollection("products")
   
+  // responisve yapıda ekran küçüldüğünde kullanılacak filtreyi gösterme gizleme butonu
+  const [showFilter,setShowFilter] = useState(false)
+
   const products = useSelector(selectProducts);
   // console.log(products)
 
@@ -31,14 +35,24 @@ const Product = () => {
     );
   }, [dispatch, data]);
 
+  const toggleFilter = () => {
+    setShowFilter(!showFilter)
+  }
+
   return (
     <section>
       <div className={`container ${styles.product}`}>
-        <aside className={styles.filter}>
+        <aside className={showFilter ? `${styles.filter}${styles.show}` : `${styles.filter}`}>
           {isLoading ? null : <ProductFilter/>}
         </aside>
         <div className={styles.content}>
           {isLoading ? (<img src={spinnerImg} alt="Loading.." style={{width: "50px"}} className="--center-all"/>) : (<ProductList products={products}/>)}
+          <div className={styles.icon} onClick={toggleFilter}>
+            <FaCogs size={20} color="orangered" />
+              <p>
+                <b>{showFilter ? "Hide Filter" : "Show Filter"}</b>
+              </p>
+          </div>
         </div>
       </div>
     </section>
