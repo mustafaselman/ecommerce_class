70215b8ec0ba5373b59c468b91e645b1b637e5f7
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-07 09:03:32 +0300

    63-2-components/product/productFilter/Pro.Fil..js

--- src/components/product/productFilter/ProductFilter.js
+++ src/components/product/productFilter/ProductFilter.js
@@ -1,14 +1,38 @@
 //// home kısmında ürün filtreleme yapılan sol yer
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { FILTER_BY_CATEGORY } from '../../../redux/slice/filterSlice'
+import { selectProducts } from '../../../redux/slice/productSlice'
 import styles from "./ProductFilter.module.scss"
 
 const ProductFilter = () => {
+
+  const [category, setCategory] = useState("All")
+
+  const products = useSelector(selectProducts)
+
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    "All",
+    ...new Set(products.map((product) => product.category))
+  ]
+
+  const filterProducts = (cat) => {
+    setCategory(cat)
+    dispatch(FILTER_BY_CATEGORY({products, category:cat}))
+  }
+
   return (
     <div className={styles.filter}>
       <h4>Categories</h4>
       <div className={styles.category}>
-        {/* burası dinamik olacak o yüzden diğerleri yazılmadı */}
-        <button>All</button>
+        {allCategories.map((cat,index) => {
+          return (
+            <button key={index} type="button" className={`${category}` === cat ? `${styles.active}` : null }
+            onClick={()=>filterProducts(cat)}>&#8250; {cat}</button>
+          )
+        })}
       </div>
       <h4>Brand</h4>
       <div className={styles.brand}>
