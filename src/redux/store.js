3c93ca31350b56ca52861708089a48b15165b09e
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-08 14:38:55 +0300

    70-2- (redux/store.js)

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import {configureStore, combineReducers} from "@reduxjs/toolkit"
 import authReducer from "./slice/authSlice"
 import productReducer from "./slice/productSlice"
 import filterReducer from "./slice/filterSlice"
+import cartReducer from "./slice/cartSlice"
 
 // burası reucerlarımızı ekleyeceğimiz yer
 const rootReducer = combineReducers({
     auth: authReducer,
     product: productReducer,
     filter: filterReducer,
+    cart: cartReducer,
 });
 
 // burade reducerlar store a kaydediliyor.
