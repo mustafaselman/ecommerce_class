ae5487a9a1d79a156a15814df807072424ece5b0
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-08 17:38:02 +0300

    72-2- (pages/cart/Cart.js)

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,9 +1,101 @@
 //// alışveriş sepeti (cart) sayfası
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { selectCartItems, selectCartTotalAmount, selectCartTotalQuantity } from '../../redux/slice/cartSlice'
+import styles from "./Cart.module.scss"
+import {FaTrashAlt} from "react-icons/fa"
+import Card from '../../components/card/Card'
 
 const Cart = () => {
+
+  const cartItems = useSelector(selectCartItems)
+  const cartTotalAmount = useSelector(selectCartTotalAmount)
+  const cartTotalQuantity = useSelector(selectCartTotalQuantity)
+
   return (
-    <div>Cart</div>
+    <section>
+      <div className={`container ${styles.table}`}>
+        <h2>Shopping Cart</h2>
+        {cartItems.length === 0 ? (
+          <>
+          <p>Your cart is currently empty.</p>
+          <br/>
+          <div>
+            <Link to="/#products">&larr; Continue shopping</Link>
+          </div>
+          </>
+        ) : (
+          <>
+          <table>
+            <thead>
+              <tr>
+                <th>s/n</th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItems.map((cart,index) => {
+                const {id,name,price,imageURL,cartQuantity} = cart;
+                return(
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <p>
+                        <b>
+                          {name}
+                        </b>
+                      </p>
+                      <img src={imageURL} alt={name} style={{width: "100px" }}/>
+                    </td>
+                    <td>{price}</td>
+                    <td>
+                      <div className={styles.count}>
+                        <button className='--btn'>-</button>
+                        <p>
+                          <b>
+                            {cartQuantity}
+                          </b>
+                        </p>
+                        <button className='--btn'>+</button>
+                      </div>
+                    </td>
+                    <td>{(price * cartQuantity).toFixed(2)}</td>
+                    <td className={styles.icons}>
+                      <FaTrashAlt size={19} color="red"/>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+          <div className={styles.summary}>
+            <button className={"--btn --btn-danger"}>Clear Cart</button> 
+            <div className={styles.checkout}>
+              <div>
+                <Link to="/#products">&larr; Continue shopping</Link>
+              </div>
+              <br/>
+              <Card cardClass={styles.card}>
+                <p><b>{`Cart item(s): ${cartTotalQuantity}`}</b></p>
+                <div className={styles.text}>
+                  <h4>Subtotal:</h4>
+                  <h3>{`$${cartTotalAmount.toFixed(2)}`}</h3>
+                </div>
+                <p>Tax an shipping calculated at checkout</p>
+                <button className="--btn --btn-primary --btn-block">Checkout</button>
+              </Card>
+            </div>
+          </div>
+          </>
+        )}
+      </div>
+     
+    </section>
   )
 }
 
