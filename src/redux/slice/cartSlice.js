b96a42d0f3045cbddcac64965fbd96b5b53d5904
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-08 14:37:42 +0300

    70-1- (redux/slice/cartSlice.js)

--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -2,15 +2,26 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-
+  cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
 }
 
 const cartSlice = createSlice({
-  name: second,
+  name: "cart",
   initialState,
-  reducers: {}
+  reducers: {
+    ADD_TO_CART(state,action) {
+      console.log(action.payload)
+      
+  },
+  }
 });
 
-export const {} = cartSlice.actions
+export const {ADD_TO_CART} = cartSlice.actions
+
+export const selectCartItems = (state) => state.cart.cartItems;
+export const selectCartTotalQuantity = (state) => state.cart.cartTotalQuantity;
+export const selectCartTotalAmount = (state) => state.cart.cartTotalAmount;
 
 export default cartSlice.reducer
