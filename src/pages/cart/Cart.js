e788a28238f6a26474675966dd384f3e4edf13d5
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-09 16:59:44 +0300

    80-2- (pages/cart/Cart.js)

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,11 +1,12 @@
 //// alışveriş sepeti (cart) sayfası
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { ADD_TO_CART, CALCULATE_SUBTOTAL, CALCULATE_TOTAL_QUANTITY, CLEAR_CART, DECREASE_CART, REMOVE_FROM_CART, selectCartItems, selectCartTotalAmount, selectCartTotalQuantity } from '../../redux/slice/cartSlice'
+import { Link, useNavigate } from 'react-router-dom'
+import { ADD_TO_CART, CALCULATE_SUBTOTAL, CALCULATE_TOTAL_QUANTITY, CLEAR_CART, DECREASE_CART, REMOVE_FROM_CART, SAVE_URL, selectCartItems, selectCartTotalAmount, selectCartTotalQuantity } from '../../redux/slice/cartSlice'
 import styles from "./Cart.module.scss"
 import {FaTrashAlt} from "react-icons/fa"
 import Card from '../../components/card/Card'
+import { selectIsLoggedIn } from '../../redux/slice/authSlice'
 
 const Cart = () => {
 
@@ -13,6 +14,9 @@ const Cart = () => {
   const cartTotalAmount = useSelector(selectCartTotalAmount)
   const cartTotalQuantity = useSelector(selectCartTotalQuantity)
   const dispatch = useDispatch()
+  const isLoggedIn = useSelector(selectIsLoggedIn)
+  const navigate = useNavigate()
+  const url = window.location.href
 
   const increaseCart = (cart) => {
     dispatch(ADD_TO_CART(cart))
@@ -31,6 +35,19 @@ const Cart = () => {
     dispatch(CALCULATE_SUBTOTAL())
     dispatch(CALCULATE_TOTAL_QUANTITY())
   },[dispatch,cartItems])
+
+  useEffect(()=> {
+    dispatch(SAVE_URL(url))
+  },[dispatch,url])
+
+  const checkout = () => {
+    if(isLoggedIn) {
+      navigate("/checkout-details")
+    }
+    else {
+      navigate("/login")
+    }
+  }
 
   return (
     <section>
@@ -106,7 +123,7 @@ const Cart = () => {
                   <h3>{`$${cartTotalAmount.toFixed(2)}`}</h3>
                 </div>
                 <p>Tax an shipping calculated at checkout</p>
-                <button className="--btn --btn-primary --btn-block">Checkout</button>
+                <button className="--btn --btn-primary --btn-block" onClick={checkout}>Checkout</button>
               </Card>
             </div>
           </div>
