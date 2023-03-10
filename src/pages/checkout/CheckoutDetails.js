08ada6ca0434a01961a9806fd86e5ba07e0f6a8a
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-10 11:45:10 +0300

    82-1-(pages/checkout/CheckoutDetails.js)

--- src/pages/checkout/CheckoutDetails.js
+++ src/pages/checkout/CheckoutDetails.js
@@ -1,9 +1,62 @@
 //// bu sayfa myorders sayfasından checkout a tıkladığımızda gelen, checkoutdetails componentidir. içinde adres inputları ve checkoutsummary yer alır.
-import React from 'react'
+import React, { useState } from 'react'
+import Card from '../../components/card/Card'
+import styles from "./CheckoutDetails.module.scss"
+
+const initialAddressState = {
+  name: "",
+  line1: "",
+  line2: "",
+  city: "",
+  state: "",
+  postal_code: "",
+  country: "",
+  phone: ""
+}
 
 const CheckoutDetails = () => {
+
+  const [shippingAddress,setShippingAdress] = useState({...initialAddressState})
+  const [billingAddress,setBillingAdress] = useState({...initialAddressState})
+  
+  const handleShipping = () => {
+  
+  };
+  const handleBilling = () => {
+    
+  };
+  const handleSubmit = () => {
+   
+  };
+
   return (
-    <div>CheckoutDetails</div>
+    <section>
+      <div className={`container ${styles.checkout}`}>
+        <h2>Checkout Details</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Card cardClass={styles.card}>
+              <h3>Shipping Address</h3>
+              <label>Recipient Name</label>
+              <input type="text" placeholder="Recipient Name" required name="name" value={shippingAddress.name} onChange={(e) => handleShipping(e)} />
+              <label>Address line 1</label>
+              <input type="text" placeholder="Address line 1" required name="line1" value={shippingAddress.line1} onChange={(e) => handleShipping(e)} />
+              <label>Address line 2</label>
+              <input type="text" placeholder="Address line 2" name="line2" value={shippingAddress.line2} onChange={(e) => handleShipping(e)} />
+              <label>City</label>
+              <input type="text" placeholder="City" required name="city" value={shippingAddress.city} onChange={(e) => handleShipping(e)} />
+              <label>State</label>
+              <input type="text" placeholder="State" required name="state" value={shippingAddress.state} onChange={(e) => handleShipping(e)} />
+              <label>Postal Code</label>
+              <input type="text" placeholder="Postal Code" required name="postal_code" value={shippingAddress.postal_code} onChange={(e) => handleShipping(e)} />
+              {/* country kısmında tüm ülkeleri çekebileceğimiz bir modül kullanacağız */}
+              <label>Phone</label>
+              <input type="text" placeholder="Phone" required name="phone" value={shippingAddress.phone} onChange={(e) => handleShipping(e)} />
+            </Card>
+          </div>
+        </form>
+      </div>   
+    </section>
   )
 }
 
