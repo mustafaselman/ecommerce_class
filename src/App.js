d8f817028a8ba672c70feb098e87f961a4f8b591
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-12 21:06:53 +0300

    84-3- (App.js)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 import AdminOnlyRoute from "./components/adminOnlyRoute/AdminOnlyRoute";
 import ProductDetails from "./components/product/productDetails/ProductDetails";
 import CheckoutDetails from "./pages/checkout/CheckoutDetails";
+import Checkout from "./pages/checkout/Checkout";
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
           <Route path="/product-details/:id" element={<ProductDetails/>} />
           <Route path="/cart" element={<Cart/>} />
           <Route path="/checkout-details" element={<CheckoutDetails/>} />
+          <Route path="/checkout" element={<Checkout/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
