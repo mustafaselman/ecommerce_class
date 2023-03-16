34bdeaf19e0a747aced06b4aff1ee6f80e2a31df
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-16 13:18:46 +0300

    90-1- components/checkoutForm/CheckoutForm.js

--- src/components/checkoutForm/CheckoutForm.js
+++ src/components/checkoutForm/CheckoutForm.js
@@ -1,10 +1,106 @@
 //// cart sayfasından checkout sayfasına ulaşıp, checkout detaylarını girip, proceed to checkout a bastığımızda karşımıza çıkan iki card a sahip check out componenti. checkoutsummary componenti de ayrı bir dosyada oluşturulmuştur.
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import {
+  PaymentElement,
+  LinkAuthenticationElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import styles from "./CheckoutForm.module.scss";
+import Card from "../card/Card";
+import CheckoutSummary from "../checkoutSummary/CheckoutSummary";
+import spinnerImg from "../../assets/spinner.gif";
+import { toast } from "react-toastify";
 
 const CheckoutForm = () => {
-  return (
-    <div>CheckoutForm</div>
-  )
-}
+  const stripe = useStripe();
+  const elements = useElements();
 
-export default CheckoutForm
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage(null);
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const confirmPayment = await stripe
+      .confirmPayment({
+        elements,
+        redirect: "if_required",
+      })
+      .then((result) => {
+        if (result.error) {
+          toast.error(result.error.message);
+          setMessage(result.error.message);
+          return;
+        }
+        if (result.paymentIntent) {
+          if (result.paymentIntent.status === "succeeded") {
+            setIsLoading(false);
+            toast.success("Payment successful");
+            saveOrder();
+          }
+        }
+      });
+    setIsLoading(false);
+  };
+
+  const saveOrder = () => {
+    toast.success("Successfully save order");
+  };
+
+  const paymentElementOptions = {
+    layout: "tabs",
+  };
+
+  return (
+    <section>
+      <div className={`container ${styles.checkout}`}>
+        <h2>Checkout</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Card cardClass={styles.card}>
+              <CheckoutSummary />
+            </Card>
+          </div>
+          <div>
+            <Card cardClass={styles.card}>
+              <h3>Stripe Checkout</h3>
+              <PaymentElement
+                id={styles["payment-element"]}
+                options={paymentElementOptions}
+              />
+              <button
+                disabled={isLoading || !stripe || !elements}
+                type="submit"
+                className={styles.button}
+              >
+                <span>
+                  {isLoading ? (
+                    <img
+                      src={spinnerImg}
+                      alt="Loading..."
+                      style={{ width: "20px" }}
+                    />
+                  ) : (
+                    "Pay now"
+                  )}
+                </span>
+              </button>
+              {/* Show any error or success messages */}
+              {message && <div id={styles["payment-message"]}>{message}</div>}
+            </Card>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default CheckoutForm;
