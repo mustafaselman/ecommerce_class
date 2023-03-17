29a8c6bd6d9403df580196a6b6589f39ef60a0c9
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-17 10:43:44 +0300

    94-2- (App.js)

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import ProductDetails from "./components/product/productDetails/ProductDetails";
 import CheckoutDetails from "./pages/checkout/CheckoutDetails";
 import Checkout from "./pages/checkout/Checkout";
 import CheckoutSuccess from "./pages/checkout/CheckoutSuccess";
+import OrderDetails from "./pages/orderDetails/OrderDetails";
+import ReviewProducts from "./components/reviewProducts/ReviewProducts";
 
 const App = () => {
   return (
@@ -29,6 +31,8 @@ const App = () => {
           <Route path="/checkout" element={<Checkout/>}/>
           <Route path="/checkout-success" element={<CheckoutSuccess/>}/>
           <Route path="/order-history" element={<OrderHistory/>}/>
+          <Route path="/order-details/:id" element={<OrderDetails/>}/>
+          <Route path="/review-product/:id" element={<ReviewProducts/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
