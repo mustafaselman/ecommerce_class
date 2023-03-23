7310a1a8189b802033ec4f16c3c6b51c62efcda2
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-23 12:25:49 +0300

    103-1- (components/admin/home/Home.js)

--- src/components/admin/home/Home.js
+++ src/components/admin/home/Home.js
@@ -9,6 +9,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { selectProducts, STORE_PRODUCTS } from '../../../redux/slice/productSlice'
 import { CALC_TOTAL_ORDER_AMOUNT, selectOrderHistory, selectTotalOrderAmount, STORE_ORDERS } from '../../../redux/slice/orderSlice'
 import useFetchCollection from '../../../customHooks/useFetchCollection'
+import Chart from '../../chart/Chart'
+
 
 const earningIcon = <AiFillDollarCircle size={30} color="#b624ff" />
 const productIcon = <BsCart4 size={30} color="#1f93ff" />
@@ -40,6 +42,9 @@ const Home = () => {
         <InfoBox cardClass={`${styles.card} ${styles.card1}`} title={"Earnings"} count={`$${totalOrderAmount}`} icon={earningIcon} />
         <InfoBox cardClass={`${styles.card} ${styles.card2}`} title={"Products"} count={products.length} icon={productIcon} />
         <InfoBox cardClass={`${styles.card} ${styles.card3}`} title={"Orders"} count={orders.length} icon={ordersIcon} />
+      </div>
+      <div>
+        <Chart />
       </div>
     </div>
   )
