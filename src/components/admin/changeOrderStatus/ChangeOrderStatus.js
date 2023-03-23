4bc55f3c0748a70fc237771bda24e9fdba89f4a0
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-03-23 09:22:39 +0300

    101-2- (components/admin/changeOrderStatus)

--- src/components/admin/changeOrderStatus/ChangeOrderStatus.js
+++ src/components/admin/changeOrderStatus/ChangeOrderStatus.js
@@ -1,10 +1,47 @@
 //// admin panelindeki order componentinin içindeki order status kısmına tıklayınca çıkan order details komponentinin içindeki update status
-import React from 'react'
+import React, { useState } from "react";
+import Card from "../../card/Card";
+import Loader from "../../loader/Loader";
+import styles from "./ChangeOrderStatus.module.scss"
 
-const ChangeOrderStatus = () => {
+const ChangeOrderStatus = ({order,id}) => {
+
+  const [status, setStatus] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const editOrder = () => {}
+
   return (
-    <div>ChangeOrderStatus</div>
-  )
-}
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.status}>
+        <Card cardClass={styles.card}>
+          <h4>Update Status</h4>
+          <form onSubmit={(e) => editOrder(e, id)}>
+            <span>
+              <select
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <option value="" disabled>
+                  -- Choose One --
+                </option>
+                <option value="Order Placed...">Order Placed...</option>
+                <option value="Processing...">Processing...</option>
+                <option value="Shipped...">Shipped...</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </span>
+            <span>
+              <button type="submit" className="--btn --btn-primary">
+                Update Status
+              </button>
+            </span>
+          </form>
+        </Card>
+      </div>
+    </>
+  );
+};
 
-export default ChangeOrderStatus
+export default ChangeOrderStatus;
